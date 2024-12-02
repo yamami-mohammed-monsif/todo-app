bad75fe97e5bacd5445043ff07fdf74b2d92eef1
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-12-02 17:24:03 +0000

    made the bg image change automatically when the screen size gets smaller, also made the page responsive

--- src/components/backgroundImage/BackgroundImage.jsx
+++ src/components/backgroundImage/BackgroundImage.jsx
@@ -1,11 +1,15 @@
 import "./background-image.css";
 
-import bgImage from "../../assets/images/bg-desktop-light.jpg";
+import bgImageDesktopLight from "../../assets/images/bg-desktop-light.jpg";
+import bgImageMobileLight from "../../assets/images/bg-mobile-light.jpg";
 
 function BackgroundImage() {
   return (
     <div className="bg-image-container">
-      <img src={bgImage} alt="Background Image" />
+      <picture>
+        <source srcSet={bgImageDesktopLight} media="(min-width: 700px)" />
+        <img src={bgImageMobileLight} alt="Background Image" />
+      </picture>
     </div>
   );
 }
